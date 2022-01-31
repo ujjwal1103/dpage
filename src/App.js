d8daa646de7093430cf8036931b707133c9ff168
Ujjwal Lade <ujjwallade11@gmail.com>
2022-01-31 13:24:08 +0530

    first comiit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hii this is first react app deployed
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hows it
         </a>
       </header>
     </div>
